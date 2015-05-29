6ad26adf0531d837cbba91844b7196863f211ef6
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-05-29 12:01:10 +0200

    Fix erreur d'indicage

--- src/conv.c
+++ src/conv.c
@@ -23,15 +23,11 @@ void YCbCr_to_ARGB(uint8_t  *mcu_YCbCr[3], uint32_t *mcu_RGB,
    /* R = Y − 0.0009267 × (Cb − 128) + 1.4016868 × (Cr − 128) */
    /* G = Y − 0.3436954 × (Cb − 128) − 0.7141690 × (Cr − 128) */
    /* B = Y + 1.7721604 × (Cb − 128) + 0.0009902 × (Cr − 128) */
-   uint32_t k = 0;		/* Indice courant de la mcu RGB */
 
-   for (uint32_t i = 0; i < MCU_BASE * nb_blocks_v; i++) {
-      for (uint32_t j = 0; j < MCU_BASE * nb_blocks_h; j++) {
-	 mcu_RGB[k] = 0;	/* Mise à 0, nécessaire pour A */
-	 mcu_RGB[k] |= R (mcu_YCbCr[0], mcu_YCbCr[1], mcu_YCbCr[2]) << 16;
-	 mcu_RGB[k] |= G (mcu_YCbCr[0], mcu_YCbCr[1], mcu_YCbCr[2]) << 8;
-	 mcu_RGB[k] |= B (mcu_YCbCr[0], mcu_YCbCr[1], mcu_YCbCr[2]);
-	 ++k;
-      }
+   for (uint32_t i = 0; i < MCU_BASE * nb_blocks_v * nb_blocks_h; i++) {
+      mcu_RGB[i]  = 0;	/* Mise à 0, nécessaire pour A */
+      mcu_RGB[i] |= R (mcu_YCbCr[0][i], mcu_YCbCr[1][i], mcu_YCbCr[2][i]) << 16;
+      mcu_RGB[i] |= G (mcu_YCbCr[0][i], mcu_YCbCr[1][i], mcu_YCbCr[2][i]) << 8;
+      mcu_RGB[i] |= B (mcu_YCbCr[0][i], mcu_YCbCr[1][i], mcu_YCbCr[2][i]);
    }
 }

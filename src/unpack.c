e3d65ae9edc16e3441f09778ba4f6d014d4f961a
Cléa Martinez <cleamartinez@hotmail.fr>
2015-06-01 10:46:31 +0200

    maj unpack

--- src/unpack.c
+++ src/unpack.c
@@ -1,6 +1,31 @@
 #include "unpack.h"
 #include <stdio.h>
 
+#define BITMASK(n) (0xFFFFFFFF)>>(32-(n))
+
+// permet de retrouver une valeur à partir d'une magnitude
+int32_t decode_magnitude(uint8_t magnitude, struct bitstream *stream,
+			 uint32_t dest, bool byte_stuffing){
+
+   // on lit le nombre de bits correspondant à la magnitude
+   uint8_t mag = read_bitstream(stream, magnitude, &dest, byte_stuffing);
+   dest &= BITMASK(magnitude);
+
+   // on décode la valeur dans la classe de magnitude
+   uint8_t premier_bit = dest >> (magnitude-1);
+   printf("pbit  %i ", premier_bit);
+   if (premier_bit ==1){
+      printf("dest %i ", dest);
+      return dest ;
+   }
+   else {
+      printf ("dest %i ", dest);
+      return ( ~dest);
+   }
+}
+
+
+
 void unpack_block(struct bitstream *stream,
 		  struct huff_table *table_DC, int32_t *pred_DC,
 		  struct huff_table *table_AC,
@@ -17,7 +42,10 @@ void unpack_block(struct bitstream *stream,
 // récupération de la magnitude
    printf("decode dc ");
    uint8_t magnitude = next_huffman_value(table_DC,stream);
-   int8_t dc= next_huffman_value(table_DC,stream)-*pred_DC;
+   uint32_t dest;
+   bool byte_stuffing=true;
+
+   int8_t dc= decode_magnitude(magnitude,stream, dest, byte_stuffing)-*pred_DC;
    bloc[0]=dc;
 
    printf("dc décodé \n");
@@ -44,8 +72,7 @@ void unpack_block(struct bitstream *stream,
 
       }
 
-      // on récupère le symbole précédant le coeff
-      //symbole=next_huffman_value(table_AC, stream);
+
 
       // on récupère le nombre de zéros et on remplit le bloc
       nb_zeros=(symbole>>4)&0x0F;
@@ -53,19 +80,18 @@ void unpack_block(struct bitstream *stream,
 	 bloc[i+j]=0;
       }
       i=i+nb_zeros ;
-      printf("nb zeros : %i ", nb_zeros);
-      printf("izéro : %i ",i);
+      //printf("nb zeros : %i ", nb_zeros);
+      //printf("izéro : %i ",i);
 
       // on récupère la magnitude du symbole
       magnitude=(symbole & 0x0F);
 
       // on lit le coefficient
-      uint32_t next=next_huffman_value(table_AC, stream);
-      bloc[i]=next;
-
+      bloc[i]=decode_magnitude(magnitude,stream,dest, byte_stuffing);
+      printf("bloc %i, magnitude %i \n", bloc[i], magnitude);
       i++;
 
-      printf("fb i= %i \n",i);
+      //printf("fb i= %i \n",i);
 
    }
 

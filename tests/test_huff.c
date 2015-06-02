5c139d4cbe2ceab3519465e8c42adc18bacf03af
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-06-03 00:20:31 +0200

    Implementation dans fichier de test des fonctions de huff
    Comme je savais que Cléou n’allait pas avoir envie de se remettre à
    Huffman demain matin, et que j’en ai parlé avec Claire ce soir, j’ai
    décidé de prendre les choses dans mes petites paluches. Heureusement,
    comme je suis un pur génie, j’ai réussi easy peasy, grâce à Otis
    Redding.

--- tests/test_huff.c
+++ tests/test_huff.c
@@ -36,7 +36,7 @@ void affiche_huffman_rec (struct abr *huff, uint8_t code[16], uint8_t nbits)
 void affiche_huffman (struct abr *huff)
 {
    if (huff) {
-      uint8_t code[16];
+      uint8_t code[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
       uint8_t nbits = 0;
       affiche_huffman_rec (huff, code, nbits);
    }
@@ -46,14 +46,10 @@ void libere_huffman (struct abr *huff)
 {
    if (huff) {
       /* libere sous-arbre gauche */
-      if (huff->gauche) {
-	 libere_huffman (huff->gauche);
-      }
+      libere_huffman (huff->gauche);
 
       /* Libere sous-arbre droit */
-      if (huff->droite) {
-	 libere_huffman (huff->droite);
-      }
+      libere_huffman (huff->droite);
 
       free (huff);
       huff = NULL;
@@ -62,91 +58,101 @@ void libere_huffman (struct abr *huff)
 
 
 
-// insertion à la profondeur prof, le plus a gauche possible
-// renvoie une booléen qui permet de savoir si l'insertion a réussi
-// prérequis : profondeur inf ou égale à lg max !!!!
-bool insertion_gauche( struct abr *abr, uint8_t prof, uint8_t symbole){
+bool insertion(struct abr **abr, uint8_t depth, uint8_t sym)
+{
+   /* On descend jusqu'à la profondeur à atteindre */
+   /* printf ("depth: %d\t", depth); */
+   /* printf ("abr: %p\n", (*abr)); */
+   if (depth) {
 
+      if (!(*abr)) {
+	 /* printf ("Création non-feuille: \n"); */
+	 /* Création d'un noeud intermédiaire */
+	 (*abr) = malloc (sizeof(struct abr));
+	 (*abr)->est_feuille = false;
+	 (*abr)->gauche = NULL;
+	 (*abr)->droite = NULL;
+      }
 
-   if (prof==1){
-      printf("prof=1 \n");
-      if(abr==NULL){
-	 abr=malloc(sizeof(struct abr));
-	 insertion_gauche(abr, prof, symbole);
-	 abr->est_feuille=false;
+      if (!(*abr)->est_feuille) {
+	 /* Tentative d'insertion dans le sous-arbre, avec priorité à gauche */
+	 if (insertion (&((*abr)->gauche), depth - 1, sym)) return true;
+	 else return insertion (&((*abr)->droite), depth - 1, sym);
+      }
+
+   } else {
+
+      /* Si le noeud est inexistant, on créée la feuille ici, sinon on tente un autre sous-arbre*/
+      if (!(*abr)) {
+	 /* printf ("Création feuille: 0x%x \t %p\n", sym, abr); */
+	 (*abr) = malloc (sizeof(struct abr));
+	 (*abr)->est_feuille = true;
+	 (*abr)->sym = sym;
+	 (*abr)->gauche = NULL;
+	 (*abr)->droite = NULL;
 	 return true;
       }
-      else if (abr->gauche ==NULL){
-	 abr->gauche= malloc(sizeof(struct abr));
-	 printf("arbre alloué g \n");
-	 abr->gauche->sym=symbole;
-	 abr->gauche->est_feuille=true;
-	 return true ;
-      }
-      else if (abr->droite ==NULL){
-	 abr->droite=malloc(sizeof(struct abr));
-	 printf("arbre alloué d \n ");
-	 abr->droite->sym=symbole;
-	 abr->droite->est_feuille =true;
-	 return true ;
-      }
-      else {
-	 printf ("niveau plein");
-	 return(false);
-      }
-      printf("sortie boucle1 \n");
-   }
-
-   else {
-      printf ("prof diff1 \n");
-      if (abr==NULL){
-	 printf("alloc nv noeud non feuille \n");
-	 abr=malloc(sizeof(struct abr));
-	 insertion_gauche(abr, prof, symbole);
-      }
-      else {
-	 printf("acs arbre non null \n");
-	 if (insertion_gauche(abr->gauche, prof-1, symbole)){
-	    printf("appel gauche \n");
-	    return true; }
-
-	 else if (insertion_gauche(abr->droite, prof-1, symbole)){
-	    printf("appel droite \n");
-	    return true;
-	 }
-	 else {
-	    printf("pas de place pour descendre");
-	    return false ;
-	 }
-      }
 
    }
-
+   return false;
 }
-   int main(void)
-   {
-      struct abr *ht = malloc (sizeof(struct abr));
-      ht->est_feuille = false;
-      //ht->gauche = malloc (sizeof(struct abr));
-      //ht->gauche->est_feuille = true;
-      //ht->gauche->gauche = ht->gauche->droite = NULL;
-      //ht->gauche->sym = 0xc1;
-      ht->droite = malloc (sizeof(struct abr));
-      ht->droite->est_feuille = false;
-      ht->droite->gauche = malloc (sizeof(struct abr));
-      ht->droite->gauche->est_feuille = true;
-      ht->droite->gauche->gauche = ht->droite->gauche->droite = NULL;
-      ht->droite->gauche->sym = 0xea;
-      ht->droite->droite = malloc (sizeof(struct abr));
-      ht->droite->droite->est_feuille = true;
-      ht->droite->droite->gauche = ht->droite->droite->droite = NULL;
-      ht->droite->droite->sym = 0x33;
 
-      affiche_huffman (ht);
-      printf("insertion \n");
-      insertion_gauche(ht, 2, 0x12);
-      affiche_huffman(ht);
-      libere_huffman (ht);
+void rec_parcours_abr(struct abr *arbre, char *code, uint8_t c, uint8_t *symbole) {
+   if (!arbre) {
+      if (arbre->est_feuille) {
+	 *symbole = arbre -> sym;
+	 return ;
+      }
 
-      return 0;
+      if (code[c] == '1') {
+	 c++;
+	 rec_parcours_abr(arbre->droite, code, c, symbole);
+      } else if (code[c] == '0') {
+	 c++;
+	 rec_parcours_abr(arbre->gauche, code, c, symbole);
+      } else {
+	 printf("erreur dans la lecture bit !=0ou1\n");
+	 exit(1);
+      }
+
    }
+}
+
+int8_t next_huffman_value(struct abr *table, char *code)
+{
+   char *direction = code;
+   uint8_t symb ;
+   rec_parcours_abr (table, direction, 0, &symb);
+   return symb;
+}
+
+int main(void)
+{
+   struct abr *ht = NULL;
+
+   printf ("INSERTIONS\n");
+   insertion (&ht, 2, 0x0d);
+   insertion (&ht, 2, 0x0e);
+   insertion (&ht, 2, 0x0a);
+   insertion (&ht, 3, 0x0c);
+   insertion (&ht, 3, 0x0b);
+
+   affiche_huffman (ht);
+
+   printf ("\nNEXT_HUFFMAN_VALUE\n");
+   char *code = "00";
+   printf ("%s <=> 0x%x\n", code, next_huffman_value  (ht, code));
+   code = "01";
+   printf ("%s <=> 0x%x\n", code, next_huffman_value  (ht, code));
+   code = "10";
+   printf ("%s <=> 0x%x\n", code, next_huffman_value  (ht, code));
+   code = "110";
+   printf ("%s <=> 0x%x\n", code, next_huffman_value  (ht, code));
+   code = "111";
+   printf ("%s <=> 0x%x\n", code, next_huffman_value  (ht, code));
+
+
+   libere_huffman (ht);
+
+   return 0;
+}

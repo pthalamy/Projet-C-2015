a0fa8c6a83aa660a7b61946dd94699f42d91f6fd
Cléa Martinez <cleamartinez@hotmail.fr>
2015-06-05 14:34:27 +0200

    fonctions de contraction adaptées aux blocs classiques. elles marchent sur les petits blocs, vérifier que le passage au grands blocs est correct

--- encodeur/downsampling_enc.c
+++ encodeur/downsampling_enc.c
@@ -17,7 +17,7 @@ void contraction_1_to_1(uint8_t *in, uint8_t *out){
 
       uint8_t *temp=malloc(64*sizeof(uint8_t));
 
-      /* pour chaque bloc, 8*8 ->8*4 */
+      /* pour chaque bloc, 8*8 ->8*4  (les2 blocs sont a la suite)*/
       for (uint8_t i=0; i<128 ; i+=2){
 	 temp[i/2]=0.5*(in[i]+in[i+1]);
       }
@@ -34,20 +34,47 @@ void contraction_1_to_1(uint8_t *in, uint8_t *out){
       free(temp);
    };
 
+  void contraction_4_to_1(uint8_t *in, uint8_t *out){
 
-/* 4 blocs 8*8 -> 4 blocs 2*2 */
-   void contraction_4_to_1(uint8_t *in, uint8_t *out){
 
-      uint8_t *temp=malloc(32*sizeof(uint8_t));
+      /* 4blocs 8*8 = 2*(2 blocs 8*8)  */
 
-      contraction_2_to_1(in ,temp);
+      uint8_t *aux1=malloc(128*sizeof(uint8_t));
+      uint8_t *aux2=malloc(128*sizeof(uint8_t));
 
-      for (uint8_t i=0; i<4; i++){
-	 for(uint8_t j=0; j<8; j+=2){
-	    out[j+16*i]=0.5*(temp[j+16*i]+temp[j+16*i+16]);
+
+      for (uint8_t i=0; i<128; i++){
+	 aux1[i]=in[i]; //2 premiers blocs dans aux1
+	 aux2[i]=in[i+128]; //2 derniers blocs dans aux2
+	 };
+
+
+      /* Contraction sur les lignes */
+      uint8_t *temp1=malloc(64*sizeof(uint8_t));
+      uint8_t *temp2=malloc(64*sizeof(uint8_t));
+
+      contraction_2_to_1(aux1, temp1);
+      contraction_2_to_1(aux2, temp2);
+
+      /*Contraction colonnes*/
+      uint8_t *out1=malloc(32*sizeof(uint8_t));
+      uint8_t *out2=malloc(32*sizeof(uint8_t));
+
+      for (uint8_t i=0; i<8; i+=2){
+	 for(uint8_t j=0; j<8; j++){
+	    out1[j+8*i/2]=0.5*(temp1[j+8*i]+temp1[j+8*i+8]);
+	     out2[j+8*i/2]=0.5*(temp2[j+8*i]+temp2[j+8*i+8]);
 	 }
       }
-      free(temp);
+
+
+      /*juxtaposition verticale des deux blocs finaux*/
+      for (uint8_t i=0; i<32; i++){
+	 out[i]=out1[i];
+	 out[i+8]=out2[i];
+
+      }
+      free(temp1);free(temp2); free(aux1); free(aux2); free(out1); free(out2);
    };
 
 

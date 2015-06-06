2f0413b836aa2368741d4ce5cf8f4886c800e106
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-06-06 19:47:21 +0200

    write doit être void

--- encodeur/bitstream_enc.h
+++ encodeur/bitstream_enc.h
@@ -8,8 +8,8 @@ struct bitstream;
 
 extern struct bitstream *create_bitstream(const char *filename);
 
-extern uint8_t write_bitstream(struct bitstream *stream,
-			       uint8_t nb_bits, uint32_t *src);
+extern void write_bitstream(struct bitstream *stream,
+			    uint8_t nb_bits, uint32_t *src);
 
 extern void free_bitstream(struct bitstream *stream);
 

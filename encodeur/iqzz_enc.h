e9f045ba7f6dbc3a0d8cadc0d006e04fd4d5dad6
Cléa Martinez <cleamartinez@hotmail.fr>
2015-06-06 16:30:38 +0200

    matrices de quantification classiques ajoutées

--- encodeur/iqzz_enc.h
+++ encodeur/iqzz_enc.h
@@ -1,3 +1,10 @@
 #include <stdint.h>
 
-void iqzz_enc(int32_t in[64], int32_t out[64], uint8_t quantif[64]);
+enum type_quantif{
+   L,
+   C,
+};
+
+
+
+void iqzz_enc(int32_t in[64], int32_t out[64], enum type_quantif q);

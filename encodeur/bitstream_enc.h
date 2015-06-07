aa370888b08918d421eb4ceefc4c3ae9ca6c2a2a
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-06-07 12:14:24 +0200

    Pas besoin de prendre un ptr en entrée dans write_btstream

--- encodeur/bitstream_enc.h
+++ encodeur/bitstream_enc.h
@@ -9,7 +9,7 @@ struct bitstream;
 extern struct bitstream *create_bitstream(const char *filename);
 
 extern void write_bitstream(struct bitstream *stream,
-			    uint8_t nb_bits, uint32_t *src);
+			    uint8_t nb_bits, uint32_t src);
 
 extern void free_bitstream(struct bitstream *stream);
 

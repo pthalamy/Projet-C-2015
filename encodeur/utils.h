b230c3a9abdfadcef44c5c3cfb00756f8a0d03c3
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-06-07 12:13:37 +0200

    Plus une macro de masking mais 2, gauche et droite

--- encodeur/utils.h
+++ encodeur/utils.h
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BITMASK(n) (0xFFFFFFFF >> (32 - (n)))
+#define BITMASK_R(n) (0xFFFFFFFF >> (32 - (n)))
+#define BITMASK_L(n) (0xFFFFFFFF << (32 - (n)))
 
 /* Retourne un pointeur sur une zone de mémoire allouée non initialisée de taille size,
  * et leve une erreur d'execution si l'allocation echoue*/
